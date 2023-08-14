aae88b2990137ebb11639943b01f370d0480a0e1
komekopan2 <yoo.shimizu888@gmail.com>
2023-08-14 23:18:12 +0900

    CRUDのUDを作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -33,6 +33,30 @@ def create_user():
     return render_template('crud/create.html', form=form)
 
 
+@crud.route('/users/<user_id>/', methods=['GET', 'POST'])
+def edit_user(user_id):
+    form = UserForm()
+
+    user = User.query.filter_by(id=user_id).first()
+
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('crud.users'))
+    return render_template('crud/edit.html', form=form, user=user)
+
+
+@crud.route('/users/<user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('crud.users'))
+
+
 @crud.route('/sql')
 def sql():
     db.session.query(User).all()
